0c0239455ec451dcc1fc3abbc72ddf8cd504a9d5
tomazs <tomaz.starasinic@gmail.com>
2026-03-03 16:15:53 +0100

    Add flex grid layout

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Tomaž in Maša potujeta';
-export const SITE_DESCRIPTION = 'Travel blog - Po najinih poteh. Malo po Južni Koreji, nato po Japonski, spet malo po Tajski, nato pa še po Avstraliji in Novi Zelandiji.';
-export const SITE_URL = 'https://tomazinmasatrip.eu';
+export const SITE_TITLE = "Tomaž in Maša potujeta";
+export const SITE_DESCRIPTION =
+  "Travel blog - Po najinih poteh. Malo po Južni Koreji, nato po Japonski, spet malo po Tajski, nato pa še po Avstraliji in Singapurju.";
+export const SITE_URL = "https://tomazinmasatrip.eu";
